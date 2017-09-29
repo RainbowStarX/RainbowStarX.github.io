7bacf39ed383a745e805d39469a33e9fe8472fe6
〃Star <babywawastarx@gmail.com>
2017-09-29 18:40:35 -0500

    Update HtmlinnerText.user.js

--- js/HtmlinnerText.user.js
+++ js/HtmlinnerText.user.js
@@ -20,7 +20,7 @@ for (var i in styles) {
 	styles[i].remove();
 }
 var style = document.createElement('style');
-style.innerHTML = 'textarea,pre{margin:0;padding:0;outline:0;border:0;}.expandingArea{z-index:999;position:relative;border:1px solid#888;background:#fff;}.expandingArea>textarea,.expandingArea>pre{padding:5px;background:transparent;font:400 13px/16px helvetica,arial,sans-serif;white-space:pre-wrap;word-wrap:break-word;}.expandingArea>textarea{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box;width:100%;height:100px;}.expandingArea.active>textarea{overflow:hidden;position:absolute;top:0;left:0;height:100%;resize:none;}.expandingArea>pre{display:none;}.expandingArea.active>pre{display:block;visibility:hidden;}';
+style.innerHTML = 'textarea,pre{margin:0;padding:0;outline:0;border:0;}.expandingArea{z-index:999;position:relative;border:1px solid#fff;background:#fff;}.expandingArea>textarea,.expandingArea>pre{padding:5px;background:transparent;font:400 12px/15px helvetica,arial,sans-serif;white-space:pre-wrap;word-wrap:break-word;}.expandingArea>textarea{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box;width:100%;height:100px;}.expandingArea.active>textarea{overflow:hidden;position:absolute;top:0;left:0;height:100%;resize:none;}.expandingArea>pre{display:none;}.expandingArea.active>pre{display:block;visibility:hidden;}';
 document.head.appendChild(style);
 var div = document.createElement('div');
 div.setAttribute('class', 'expandingArea');
@@ -29,7 +29,7 @@ document.querySelector('.expandingArea').appendChild(document.createElement('pre
 document.querySelector('.expandingArea pre').appendChild(document.createElement('span'));
 document.querySelector('.expandingArea pre').appendChild(document.createElement('br'));
 document.querySelector('.expandingArea').appendChild(document.createElement('textarea'));
-document.querySelector('.expandingArea textarea').value = '<!--原网页加载js文件后元素源码：-->' + text;
+document.querySelector('.expandingArea textarea').value = '<!--⡞完整源码⡞-->\r\n' + text;
 function makeExpandingArea(container) {
 	var area = container.querySelector('textarea');
 	var span = container.querySelector('span');
